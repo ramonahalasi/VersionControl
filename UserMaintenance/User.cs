30d0a863c90e9d9a06d90433fc5a32312832c868
22garmy@gmail.com <22garmy@gmail.com>
2019-12-01 21:49:06 +0100

    commit4

--- UserMaintenance/User.cs
+++ UserMaintenance/User.cs
@@ -4,17 +4,11 @@ namespace UserMaintenance
     public class User
     {
         public Guid ID { get; set; } = Guid.NewGuid();
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string FullName
-        {
-            get
-            {
-                return string.Format(
-                   "{0} {1}",
-                   LastName,
-                   FirstName);
-            }
-        }
+        public string FirstName;
+        public string LastName;
+        public string FullName { get; set; }
+        
+            
+       
     }
 }
